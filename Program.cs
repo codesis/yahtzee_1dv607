8ee149638dd1706d53a7e2b619c67eabf15af1d6
codesis <hassling.emma@gmail.com>
2019-10-22 14:42:57 +0200

    starting implementation of yahtzee assignment in c#

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace yahtzee_1dv607
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
